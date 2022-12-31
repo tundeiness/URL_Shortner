8b0371926eb5200f2bcb5642bf4eca10be2e786d
tundeiness <tundeiness@yahoo.co.uk>
2022-12-31 19:19:09 +0100

    add user references

---
+++ db/migrate/20221231181747_add_user_to_url.rb
@@ -0,0 +1,5 @@
+class AddUserToUrl < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :urls, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_31_181227) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_31_181747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_31_181227) do
     t.string "original_url"
     t.string "short_url"
     t.integer "count"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_urls_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "urls", "users"
 end
