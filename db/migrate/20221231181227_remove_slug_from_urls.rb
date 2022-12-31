5edd0715f6b0ab5eac6967a29f2396817a7b4536
tundeiness <tundeiness@yahoo.co.uk>
2022-12-31 19:13:33 +0100

    remove extras columns

---
+++ db/migrate/20221231181227_remove_slug_from_urls.rb
@@ -0,0 +1,5 @@
+class RemoveSlugFromUrls < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :urls, :slug, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_31_180505) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_31_181227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "urls", force: :cascade do |t|
-    t.string "name"
-    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "original_url"
