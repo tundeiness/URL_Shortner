cf45345f93d16f48d118461ff10281b3040ed658
tundeiness <tundeiness@yahoo.co.uk>
2022-12-31 19:27:23 +0100

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'urls#index'
+  # root to: 'urls#index'
 
-  get 'r', to: 'redirects#index'
-  resources :urls, only: %i[create index]
+  # get 'r', to: 'redirects#index'
+  # resources :urls, only: %i[create index]
+  resources :users do
+    resources :urls, only: [:index, :new, :create, :show]
+  end
 end
