2b4b9e39d81375e66857265c803adc531a7a85a9
tundeiness <tundeiness@yahoo.co.uk>
2022-12-31 19:21:33 +0100

    add associations

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,5 @@
 class Url < ApplicationRecord
+  belongs_to :user
 
   validates :name, format: { with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix, message: "url is not in the right format" }
 
